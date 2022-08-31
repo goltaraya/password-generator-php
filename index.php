0e0378d28c4ff1b5774993ee4cdf874a6afe148e
Yago Alexandre Goltara Affonso <yago.affonso@tce.ma.gov.br>
2022-08-31 10:13:49 -0300

    first commit::creating templates

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include_once 'templates/header.php'; ?>
+
+<header class="header">
+    <h1 id="pg-title">Password Generator 0.1</h1>
+</header>
+
+<div>
+    <button></button>
+</div>
+
+<?php include_once 'templates/footer.php'; ?>
---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<?php
+include_once 'config/url.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/style/style.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="<?= $BASE_URL ?>img/favicon.webp" type="image/x-icon">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+</head>
+
+<body>
